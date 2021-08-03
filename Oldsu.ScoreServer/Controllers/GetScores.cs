b38568f35de7bf8c000147da2da311052631555b
jeesusmies <burnerthingxd@gmail.com>
2021-08-03 17:54:32 +0300

    Remove rank cache from GetScores.

--- Oldsu.ScoreServer/Controllers/GetScores.cs
+++ Oldsu.ScoreServer/Controllers/GetScores.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -14,19 +15,13 @@ namespace Oldsu.ScoreServer.Controllers
     [Route("/web/osu-getscores6.php")]
     public class GetScores : ControllerBase, IOsuController
     {
-        /// <summary>
-        ///     Contains a cache of total ranks in a score in the leaderboard.
-        ///     The field is going to be incremented, when a score gets submitted on the map.
-        /// </summary>
-        public static ConcurrentDictionary<(int, byte), int> TotalLeaderboardRankCache = new ();
-
         private readonly ILogger<ScoreSubmission> _logger;
 
         private User _requestingUser;
         private Beatmap _beatmap;
         private byte _gamemode;
         
-        private IAsyncEnumerable<ScoreRow> _scoresOnMap;
+        private IAsyncEnumerable<HighScoreWithRank> _scoresOnMap;
 
         public GetScores(ILogger<ScoreSubmission> logger)
         {
@@ -36,6 +31,10 @@ namespace Oldsu.ScoreServer.Controllers
         [HttpGet]
         public async Task Get()
         {
+            Stopwatch sw = new Stopwatch();
+
+            sw.Start();
+            
             var db = new Database();
             
             var userId = HttpContext.Request.Query["u"].ToString();
@@ -68,30 +67,21 @@ namespace Oldsu.ScoreServer.Controllers
 
             _gamemode = byte.Parse(HttpContext.Request.Query["m"]);
             
-            if (!TotalLeaderboardRankCache.TryGetValue((_beatmap.BeatmapID, _gamemode), out _))
-            {
-                var allScores = db.Scores
-                    .Where(s => s.BeatmapHash.Equals(mapHash) && s.Gamemode.Equals(_gamemode))
-                    .Include(s => s.User)
-                    .OrderByDescending(s => s.Score)
-                    .Distinct();
-
-                TotalLeaderboardRankCache.TryAdd((_beatmap.BeatmapID, _gamemode), allScores.Count());
-
-                _scoresOnMap = allScores
-                    .AsAsyncEnumerable();
-            }
-            else
-            {
-                _scoresOnMap = db.Scores
-                    .Where(s => s.BeatmapHash.Equals(mapHash) && s.Gamemode.Equals(_gamemode))
-                    .Include(s => s.User)
-                    .OrderByDescending(s => s.Score)
-                    .Distinct()
-                    .AsAsyncEnumerable();
-            }
+            _scoresOnMap = db.HighScoresWithRank
+                .Where(s => s.BeatmapHash.Equals(mapHash) &&
+                            s.Gamemode.Equals(_gamemode) &&
+                            s.Passed)
+                .Include(s => s.User)
+                .OrderByDescending(s => s.Score)
+                .Take(50)
+                .AsQueryable()
+                .AsAsyncEnumerable();
 
             await WriteResponse();
+            
+            sw.Stop();
+
+            Console.WriteLine("Elapsed={0}",sw.ElapsedMilliseconds);
         }
 
         public async Task WriteResponse()
@@ -110,20 +100,16 @@ namespace Oldsu.ScoreServer.Controllers
             await HttpContext.Response.WriteStringAsync(
                 $"{_beatmap.Beatmapset.RankingStatus}\n0\n \n{_beatmap.Rating}\n");
 
-            var scoresAlreadyAdded = new HashSet<string>();
-            
             var db = new Database();
             
             // get personal best score and write it to the content stream, if it doesnt exist just write /n
             // yes it's a pretty hefty query but what can you do
-            var personalBestScore = await db.Scores
+            var personalBestScore = await db.HighScoresWithRank
                 .Where(s => s.BeatmapHash.Equals(_beatmap.BeatmapHash) &&
                             s.Gamemode.Equals(_gamemode) &&
-                            s.UserId.Equals(_requestingUser.UserID))
+                            s.UserId.Equals(_requestingUser.UserID) &&
+                            s.Passed)
                 .Include(s => s.User)
-                .OrderByDescending(s => s.Score)
-                .GroupBy(s => s.UserId)
-                .Select(g => g.FirstOrDefault())
                 .FirstOrDefaultAsync();
 
             if (personalBestScore != null)
@@ -131,14 +117,10 @@ namespace Oldsu.ScoreServer.Controllers
             else
                 await HttpContext.Response.WriteStringAsync("\n");
             
-            if (TotalLeaderboardRankCache.TryGetValue((_beatmap.BeatmapID, _gamemode), out var totalScores))
-            {
-                await foreach (var score in _scoresOnMap)
-                    await HttpContext.Response.WriteStringAsync(score.ToString());
+            await foreach (var score in _scoresOnMap)
+                await HttpContext.Response.WriteStringAsync(score.ToString());
 
-                for (int i = 50; i < totalScores; i++)
-                    await HttpContext.Response.WriteStringAsync($"0|0|0|0|0|0|0|0|0|0|0|0|0|0|{i}|0");
-            }
+            await HttpContext.Response.CompleteAsync();
         }
     }
 }
