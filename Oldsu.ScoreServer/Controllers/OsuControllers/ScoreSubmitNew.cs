266760dbede9c3fed6c8bc5d2c36511b0c995ca1
jeesusmies <burnerthingxd@gmail.com>
2021-08-09 18:50:32 +0300

    Implement a ScoreSubmitManager and score-submit-new emulation. Refactoring is needed.

---
+++ Oldsu.ScoreServer/Controllers/OsuControllers/ScoreSubmitNew.cs
@@ -0,0 +1,115 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Oldsu.ScoreServer.Managers;
+using Oldsu.Types;
+using Oldsu.Utils;
+
+namespace Oldsu.ScoreServer.Controllers.OsuControllers
+{
+    [ApiController]
+    [Route("/web/osu-submit-new.php")]
+    public class ScoreSubmitNew : ControllerBase, IOsuController // score submission for atleast 2009-2012
+    {
+        private readonly ILogger<ScoreSubmission> _logger;
+        
+        public ScoreSubmitNew(ILogger<ScoreSubmission> logger)
+        {
+            _logger = logger;
+        }
+        
+        [HttpPost]
+        public async Task Post()
+        {
+            var replay = HttpContext.Request.Body;
+
+            var serializedScore = await TypeExtensions.SerializeScoreString(
+                HttpContext.Request.Query["score"].ToString().Split(":"));
+
+            await using var db = new Database();
+
+            var user = await db.AuthenticateAsync(
+                serializedScore?.User?.Username ?? "", HttpContext.Request.Query["pass"]);
+            
+            if (user == null)
+            {
+                await HttpContext.Response.WriteStringAsync(ScoreSubmitManager.WrongPasswordMessage);
+                await HttpContext.Response.CompleteAsync();
+                return;
+            }
+
+            serializedScore!.User = user;
+
+            if (user.Banned)
+            {
+                await HttpContext.Response.WriteStringAsync(ScoreSubmitManager.BannedMessage);
+                await HttpContext.Response.CompleteAsync();
+                return;
+            }
+
+            var submitManager = new ScoreSubmitManager
+                (serializedScore, await db.Beatmaps
+                    .Where(b => b.BeatmapHash == serializedScore.BeatmapHash)
+                    .Include(b => b.Beatmapset)
+                    .FirstOrDefaultAsync());
+
+            if (!submitManager.SetStrategy())
+            {
+                await HttpContext.Response.CompleteAsync();
+                return;
+            }
+
+            var (isSubmittable, responseString, bannedReason) = submitManager.ValidateScore();
+            
+            if (!isSubmittable)
+            {
+                await HttpContext.Response.WriteStringAsync(responseString);
+
+                if (responseString == ScoreSubmitManager.BannedMessage)
+                    //db.ban(username)
+                    await Task.Delay(1);
+                
+                await HttpContext.Response.CompleteAsync();
+                return;
+            }
+
+            var oldStats = await db.StatsWithRank
+                .Where(s => s.UserID == user.UserID)
+                .AsNoTracking()
+                .FirstOrDefaultAsync();
+            
+            var newStats = db.Entry(oldStats).CurrentValues.Clone().ToObject() as StatsWithRank;
+            db.Attach(newStats!);
+            
+            var oldScore = await db.HighScoresWithRank
+                .Where(s => s.BeatmapHash == serializedScore.BeatmapHash)
+                .FirstOrDefaultAsync();
+
+            await submitManager.SubmitScore();
+            
+            var newScore = await db.HighScoresWithRank                
+                .Where(s => s.BeatmapHash == serializedScore.BeatmapHash)
+                .FirstOrDefaultAsync();
+
+            var nextUserStats = await db.StatsWithRank
+                .Where(s => s.Rank == s.Rank - 1)
+                .FirstOrDefaultAsync();
+
+            submitManager.UpdateStats(newStats, oldScore);
+            
+            await newStats.SaveChangesAsync();
+
+            await HttpContext.Response.WriteStringAsync(
+                submitManager.GetScorePanelString((newScore, oldScore), (newStats, oldStats), nextUserStats));
+            
+            await HttpContext.Response.CompleteAsync();
+        }
+
+        public async Task WriteResponse()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Oldsu.ScoreServer/Managers/ScoreSubmitManager.cs
@@ -0,0 +1,100 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Oldsu.Enums;
+using Oldsu.ScoreServer.Managers.ScoreSubmissionStrategies;
+using Oldsu.Types;
+using Oldsu.Utils;
+
+namespace Oldsu.ScoreServer.Managers
+{
+    public class ScoreSubmitManager
+    {
+        public const string BannedMessage = "error: ban";
+        public const string WrongPasswordMessage = "error: pass";
+        public const string ErrorMessage = "error: no";
+
+        private IScoreSubmissionStrategy _scoreSubmissionStrategy;
+        private ScoreRow _score;
+        private Beatmap _beatmap;
+
+        public ScoreSubmitManager(ScoreRow score, Beatmap beatmap)
+        {
+            _score = score;
+            _beatmap = beatmap;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns>if a suitable version was found</returns>
+        public bool SetStrategy()
+        {
+            _scoreSubmissionStrategy = _score.Version switch
+            { // change to enums
+                "2000" => new B904ScoreSubmissionStrategy(),
+                _ => null,
+            };
+
+            return _scoreSubmissionStrategy != null;
+        }
+
+        //return value submittable and error string and ban reason
+        public (bool, string?, string?) ValidateScore()
+        {
+            var mods = (Mod)_score.Mods;
+            
+            if ((((mods & Mod.DoubleTime) == Mod.DoubleTime && (mods & Mod.HalfTime) == Mod.HalfTime)) ||
+                ((mods & Mod.HardRock) == Mod.HardRock && (mods & Mod.Easy) == Mod.Easy))
+            {
+                return (false, BannedMessage, "Invalid mods.");
+            }
+            
+            if (((mods & Mod.Relax) == Mod.Relax) || ((mods & Mod.Autoplay) == Mod.Autoplay) ||
+                ((mods & Mod.Relax2) == Mod.Relax2))
+            {
+                return (false, null, null);
+            }
+            
+            if (!_scoreSubmissionStrategy.ValidateScoreChecksum(_score))
+                return (false, BannedMessage, "Invalid score checksum.");
+                
+            //todo check score
+
+            return (true, null, null);
+        }
+
+        public string GetScorePanelString((HighScoreWithRank, HighScoreWithRank) comparableScores,
+            (StatsWithRank, StatsWithRank) comparableUsers,
+            StatsWithRank nextUser) =>
+            _scoreSubmissionStrategy.GetScorePanelString(comparableScores,
+                comparableUsers,
+                nextUser);
+
+        public async Task SubmitScore()
+        {
+            await using var db = new Database();
+
+            _score.User = null;
+            
+            db.Scores.Add(_score);
+
+            await db.SaveChangesAsync();
+        }
+
+        public void UpdateStats(StatsWithRank userStats, HighScoreWithRank? oldScore)
+        {
+            // updates other stats than ranked score ill change position, sp
+            userStats.UpdateStats(_score);
+            
+            if (_score.Passed)
+                // change to a enum instead of magic numbers
+                if (_beatmap.Beatmapset.RankingStatus == 2)
+                    if (oldScore != null && _score.Score >= oldScore.Score)
+                        // breaks single responsibility principle..? idk i dont get payed enough to care
+                        userStats.RankedScore += _score.Score - oldScore.Score;
+                    else
+                        userStats.RankedScore += _score.Score;
+        }
+    }
+}
