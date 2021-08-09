3ddaf689d9c72429f96bc2d033e6d63e90ff370f
jeesusmies <burnerthingxd@gmail.com>
2021-08-09 18:45:19 +0300

    Add score submission strategies for different versions.

---
+++ Oldsu.ScoreServer/Managers/ScoreSubmissionStrategies/B394ScoreSubmissionStrategy.cs
@@ -0,0 +1,23 @@
+﻿using Oldsu.Types;
+
+namespace Oldsu.ScoreServer.Managers.ScoreSubmissionStrategies
+{
+    public class B904ScoreSubmissionStrategy : IScoreSubmissionStrategy 
+    {
+        public bool ValidateScoreChecksum(ScoreRow scoreRow)
+        {
+            return true;
+        }
+
+        public string GetScorePanelString((HighScoreWithRank, HighScoreWithRank) comparableScores, (StatsWithRank, StatsWithRank) comparableUsers,
+            StatsWithRank? nextUser)
+        {
+            var (score, _) = comparableScores;
+
+            if (nextUser == null)
+                return $"{score.Rank}\n0\nfruitplatter.png";
+            else
+                return $"{score.Rank}\n{nextUser.RankedScore}\nfruitplatter.png";
+        }
+    }
+}
---
+++ Oldsu.ScoreServer/Managers/ScoreSubmissionStrategies/IScoreSubmissionStrategy.cs
@@ -0,0 +1,13 @@
+﻿using Oldsu.Types;
+
+namespace Oldsu.ScoreServer.Managers.ScoreSubmissionStrategies
+{
+    // the interface name is kind of misleading bc it does things like validate the score check sum too
+    public interface IScoreSubmissionStrategy
+    {
+        public bool ValidateScoreChecksum(ScoreRow scoreRow);
+        public string GetScorePanelString((HighScoreWithRank, HighScoreWithRank) comparableScores,
+                                          (StatsWithRank, StatsWithRank) comparableUsers,
+                                          StatsWithRank nextUser);
+    }
+}
