f27ce8ca9a2bcd9e05cf6bf002ac6b2c70c6c4f6
jeesusmies <burnerthingxd@gmail.com>
2021-08-21 19:51:49 +0300

    Add direct set searching and downloading.

---
+++ Oldsu.ScoreServer/Controllers/OsuControllers/DirectDownload.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Oldsu.ScoreServer.Controllers.OsuControllers
+{
+    [ApiController]
+    [Route("/d/{id}")]
+    public class DirectDownload : ControllerBase
+    {
+        private readonly ILogger<ScoreSubmission> _logger;
+        
+        public DirectDownload(ILogger<ScoreSubmission> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get(string id)
+        {
+            // todo set config file
+
+            await using var db = new Database();
+
+            var map = await db.Beatmapsets
+                .Where(b => b.BeatmapsetID == int.Parse(id))
+                .FirstOrDefaultAsync();
+
+            if (map == null)
+                return NotFound();
+            
+            // map is a bancho map
+            if (map.OriginalBeatmapsetID != null)
+                return Redirect($"https://chimu.moe/d/{id}");
+
+            // todo oldsu map
+            return Ok();
+        }
+    }
+}
---
+++ Oldsu.ScoreServer/Controllers/OsuControllers/DirectSearchSet.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using MySqlConnector;
+using Oldsu.Types;
+
+using DbFunctions = System.Data.Entity.DbFunctions;
+
+namespace Oldsu.ScoreServer.Controllers.OsuControllers
+{
+    [ApiController]
+    [Route("/web/osu-search-set.php")]
+    public class DirectSearchSet : ControllerBase
+    {
+        private readonly ILogger<ScoreSubmission> _logger;
+        
+        public DirectSearchSet(ILogger<ScoreSubmission> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task Get()
+        {
+            var requestQuery = HttpContext.Request.Query["q"].ToString();
+
+            IAsyncEnumerable<Beatmapset> mapCollections;
+
+            string k, v;
+
+            await using var db = new Database();
+
+            IQueryable<Beatmap> mapQuery = db.Beatmaps;
+
+            if (HttpContext.Request.Query.ContainsKey("s"))
+                mapQuery = mapQuery
+                    .Where(b => b.BeatmapsetID == int.Parse(HttpContext.Request.Query["s"].ToString()));
+            else if (HttpContext.Request.Query.ContainsKey("b"))
+                mapQuery = mapQuery
+                    .Where(b => b.BeatmapID == int.Parse(HttpContext.Request.Query["b"].ToString()));
+            else
+            {
+                await HttpContext.Response.CompleteAsync();
+                return;
+            }
+
+            var map = await mapQuery
+                .Include(b => b.Beatmapset)
+                .FirstOrDefaultAsync();
+
+            if (map == null)
+            {
+                await HttpContext.Response.CompleteAsync();
+                return;
+            }
+
+            await HttpContext.Response.WriteStringAsync($"\n{map.BeatmapsetID}.osz|{map.Beatmapset.Artist}|{map.Beatmapset.Title}|{map.Beatmapset.CreatorName}|" +
+                                                        $"{map.Beatmapset.RankingStatus}|{map.Beatmapset.Rating}|0|{map.BeatmapsetID}|" +
+                                                        $"0|0|0|0|0|");
+            
+            await HttpContext.Response.CompleteAsync();
+        }
+    }
+}
