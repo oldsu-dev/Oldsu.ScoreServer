c670c5599bee8c0ae66e4f2d51f6f7c584603e87
jeesusmies <burnerthingxd@gmail.com>
2021-08-22 19:45:28 +0300

    Add replay retrieval endpoint.

---
+++ Oldsu.ScoreServer/Controllers/OsuControllers/GetReplay.cs
@@ -0,0 +1,35 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Oldsu.ScoreServer.Controllers.OsuControllers
+{
+    [ApiController]
+    [Route("/web/osu-getreplay.php")]
+    public class GetReplay : Controller
+    {
+        public GetReplay()
+        {
+            
+        }
+
+        [HttpGet]
+        public async Task Get()
+        {
+            if (!int.TryParse(HttpContext.Request.Query["c"].ToString(), out var scoreId))
+            {
+                await Global.LoggingManager.LogCritical<GetReplay>
+                    ($"IP {HttpContext.GetServerVariable("HTTP_X_FORWARDED_FOR") ?? "unknown"}" +
+                     $" tried to get a replay by the unparseable id of {HttpContext.Request.Query["c"].ToString()}");
+
+                await HttpContext.Response.CompleteAsync();
+                return;
+            }
+
+            await HttpContext.Response.Body.WriteAsync(
+                await System.IO.File.ReadAllBytesAsync($"{Global.ReplayFolder}{scoreId}.osr"));
+            
+            await HttpContext.Response.CompleteAsync();
+        }
+    }
+}
---
+++ Oldsu.ScoreServer/Controllers/OsuControllers/RatingSubmit.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Oldsu.Types;
+
+namespace Oldsu.ScoreServer.Controllers.OsuControllers
+{
+    [ApiController]
+    [Route("/rating/submit-rate.php")]
+    public class SubmitRating : Controller
+    {
+        public SubmitRating()
+        {
+            
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var db = new Database();
+            
+            var beatmapHash = HttpContext.Request.Query["c"];
+            var ratingValue = HttpContext.Request.Query["v"];
+            
+            var user = await db.AuthenticateAsync(
+                HttpContext.Request.Query["u"], HttpContext.Request.Query["p"]);
+            
+            var beatmap = await db.Beatmaps
+                .Where(b => b.BeatmapHash.Equals(beatmapHash))
+                .Include(b => b.Beatmapset)
+                .FirstOrDefaultAsync();
+                
+            if (user == null || beatmap == null)
+                return Content("no");
+            
+            if (beatmap.Beatmapset.RankingStatus == 0)
+                return Content("no");
+            
+            if ((beatmap.Beatmapset.CreatorID ?? 0) == user.UserID)
+                return Content("no");
+
+            var hasRated = await db.Ratings
+                .Where(r => r.UserID == user.UserID &&
+                            r.BeatmapsetID == beatmap.BeatmapsetID)
+                .FirstOrDefaultAsync() != null;
+
+            if (hasRated)
+                return Content($"alreadyvoted\n{beatmap.Beatmapset.Rating:1}");
+
+            var allRatings = db.Ratings
+                .Where(r => r.BeatmapsetID == beatmap.BeatmapsetID)
+                .AsAsyncEnumerable();
+            
+            var ratingRow = new Rating
+            {
+                UserID = user.UserID,
+                BeatmapsetID = beatmap.BeatmapsetID,
+                Rate = float.Parse(ratingValue.ToString())
+            };
+
+            db.Add(ratingRow);
+            
+            var sumOfRatings = 0f;
+
+            await foreach (var rating in allRatings)
+                sumOfRatings += rating.Rate;
+
+            sumOfRatings += float.Parse(ratingValue.ToString());
+            
+            var newAverage = sumOfRatings / (beatmap.Beatmapset.RatingCount + 1);
+
+            beatmap.Beatmapset.Rating = newAverage;
+            beatmap.Beatmapset.RatingCount++;
+
+            await db.SaveChangesAsync();
+
+            return Content($"{newAverage}");
+        }
+    }
+}
