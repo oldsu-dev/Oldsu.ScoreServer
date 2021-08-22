8c988b46f4f472d321e1005796e6f2a9949f4277
jeesusmies <burnerthingxd@gmail.com>
2021-08-22 16:35:22 +0300

    Add preview endpoints for direct + caching.

---
+++ Oldsu.ScoreServer/Controllers/OsuControllers/PreviewImage.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Oldsu.Utils.Cache;
+
+namespace Oldsu.ScoreServer.Controllers.OsuControllers
+{
+    [ApiController]
+    [Route("/preview/images/{id}")]
+    public class PreviewImage : ControllerBase
+    {
+        private readonly ILogger<ScoreSubmission> _logger;
+        
+        private static readonly AsyncDictionaryWithExpiration<string> ResponseCache = new();
+        
+        public PreviewImage(ILogger<ScoreSubmission> logger)
+        {
+            _logger = logger;
+        }
+        
+        [HttpGet]
+        public async Task<IActionResult> Get(string id)
+        {
+            var (actionIsFound, action) = await ResponseCache.TryGetValue(id);
+
+            if (actionIsFound)
+                return action as IActionResult;
+
+            await using var db = new Database();
+            IActionResult respAction;
+
+            var map = await db.Beatmapsets
+                .Where(b => b.BeatmapsetID == int.Parse(id))
+                .FirstOrDefaultAsync();
+
+            if (map == null)
+                return NotFound();
+            
+            // map is a bancho map
+            if (map.OriginalBeatmapsetID != null)
+            {
+                respAction = Redirect($"https://b.ppy.sh/thumb/{map.OriginalBeatmapsetID}.jpg");
+                
+                await ResponseCache.TryAdd(id, respAction, DateTime.Now.AddSeconds(300));
+                return respAction;
+            }
+
+
+            // todo oldsu map
+            return Ok();
+        }
+    }
+    
+    [ApiController]
+    [Route("/preview/largeimages/{id}")]
+    public class PreviewLargeImage : ControllerBase
+    {
+        private readonly ILogger<ScoreSubmission> _logger;
+        
+        private static readonly AsyncDictionaryWithExpiration<string> ResponseCache = new();
+        
+        public PreviewLargeImage(ILogger<ScoreSubmission> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get(string id)
+        {
+            var (actionIsFound, action) = await ResponseCache.TryGetValue(id);
+
+            if (actionIsFound)
+                return action as IActionResult;
+
+            await using var db = new Database();
+            IActionResult respAction;
+
+            var map = await db.Beatmapsets
+                .Where(b => b.BeatmapsetID == int.Parse(id))
+                .FirstOrDefaultAsync();
+
+            if (map == null)
+                return NotFound();
+            
+            // map is a bancho map
+            if (map.OriginalBeatmapsetID != null)
+            {
+                respAction = Redirect($"https://b.ppy.sh/thumb/{map.OriginalBeatmapsetID}l.jpg");
+                
+                await ResponseCache.TryAdd(id, respAction, DateTime.Now.AddSeconds(300));
+                return respAction;
+            }
+
+            // todo oldsu map
+            return Ok();
+        }
+    }
+}
---
+++ Oldsu.ScoreServer/Controllers/OsuControllers/PreviewMp3.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Oldsu.Utils.Cache;
+
+namespace Oldsu.ScoreServer.Controllers.OsuControllers
+{
+    [ApiController]
+    [Route("/preview/mp3/{id}")]
+    public class PreviewMp3 : ControllerBase
+    {
+        private readonly ILogger<ScoreSubmission> _logger;
+
+        private static readonly AsyncDictionaryWithExpiration<string> ResponseCache = new();
+
+        public PreviewMp3(ILogger<ScoreSubmission> logger)
+        {
+            _logger = logger;
+        }
+        
+        [HttpGet]
+        public async Task<IActionResult> Get(string id)
+        {
+            var (actionIsFound, action) = await ResponseCache.TryGetValue(id);
+
+            if (actionIsFound)
+                return action as IActionResult;
+            
+            await using var db = new Database();
+            IActionResult respAction;
+
+            var map = await db.Beatmapsets
+                .Where(b => b.BeatmapsetID == int.Parse(id))
+                .FirstOrDefaultAsync();
+
+            if (map == null)
+                return NotFound();
+
+            // map is a bancho map
+            if (map.OriginalBeatmapsetID != null)
+            {
+                respAction = Redirect($"https://b.ppy.sh/preview/{map.OriginalBeatmapsetID}.mp3");
+                
+                await ResponseCache.TryAdd(id, respAction, DateTime.Now.AddSeconds(300));
+                return respAction;
+            }
+                
+
+            // todo oldsu map
+            return Ok();
+        }
+    }
+}
